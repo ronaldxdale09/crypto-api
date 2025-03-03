a0383aa9487e8a2a01f1c35fc0ae6ce232250d08
vryll <vryllatilano14@gmail.com>
2025-03-03 14:23:36 +0800

    getUser Information added

--- CryptoTrade/user_account/forms.py
+++ CryptoTrade/user_account/forms.py
@@ -1,11 +1,36 @@
 from ninja import Schema
 from .models import *
 from pydantic import BaseModel
+from typing import Optional, List
+from wallet.forms import *
 
 class UserSchema(Schema):
     # name: Opstr
     email: str
-    password:str
+    # password:str
+
+class UserDetailSchema(Schema):
+    phone_number: Optional[str] = None
+    secret_phrase: Optional[str] = None
+    is_verified: Optional[bool] = None
+    tier: Optional[int] = None
+    trading_fee_rate: Optional[float] = None
+    ip_address: Optional[str] = None
+    last_login_session: Optional[str] = None
+    previous_ip_address: Optional[str] = None
+    referral_code: Optional[str] = None
+    status: Optional[str] = None
+
+class UserWalletResponseSchema(Schema):
+    user: Optional[UserSchema]
+    user_detail: Optional[UserDetailSchema] = None
+    wallet: Optional[WalletSchema] = None
+
+
+class UserWalletResponseSchema(Schema):
+    user: UserSchema
+    user_detail: UserDetailSchema
+    wallet: WalletSchema
 
 class SingupUserSchema(Schema):
     email: str
--- CryptoTrade/wallet/forms.py
+++ CryptoTrade/wallet/forms.py
@@ -1,8 +1,13 @@
 from .models import *
 from ninja import Schema
+from typing import List, Optional
 
 
 class WalletBalanceSchema(Schema):
-    wallet_id:int
-    crypto_id:int
-    balance:int
+    wallet_id: Optional[int] = None
+    crypto_id: Optional[int] = None
+    network_id: Optional[int] = None
+    balance: float
+class WalletSchema(Schema):
+    wallet_id: int
+    balances: List[WalletBalanceSchema]
